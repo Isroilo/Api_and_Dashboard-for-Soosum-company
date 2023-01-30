591d3aaa3cb11898cbd545821ee4e9ab65f77ff1
Isroilo <isroilovazizbek611@gmail.com>
2023-01-30 21:56:29 +0500

    "first"

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+from .views import *
 
 
 urlpatterns = [
-
+    path("banner/", banner_view),
+    path("about_product/", about_product_view),
+    path("about-company/", about_company_view),
+    path("products/", product_view),
+    path("order/", order_add),
+    path("instruction/", instruction_view),
+    path("fact_title/", fact_title_view),
+    path("fact_item/", fact_item_view),
+    path("advice_item/", advice_item_view),
+    path("info/", info_view),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,90 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
+from .serializer import *
+
+
+@api_view(['GET'])
+def banner_view(request):
+    try:
+        banner = Banner.objects.last()
+        serializer = BannerSerializer(banner)
+        return Response(serializer.data)
+    except:
+        return Response({'message': 'None'})
+
+@api_view(['GET'])
+def about_product_view(request):
+    about_product = About_Product.objects.all().order_by('-id')
+    serializer = About_ProductSerializer(about_product, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def about_company_view(request):
+    try:
+        about_company = About_Company.objects.last()
+        serializer = About_Company(about_company)
+        return Response(serializer.data)
+    except:
+        return Response({'message': 'None'})
+
+
+@api_view(['GET'])
+def product_view(request):
+    products = Product.objects.all().order_by('id')
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def fact_title_view(request):
+    titles = Fact_Title.objects.all().order_by('-id')
+    serializer = Fact_TitleSerializer(titles, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def fact_item_view(request):
+    facts = Fact_item.objects.all().order_by('id')
+    serializer = Fact_itemSerializer(facts, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def advice_item_view(request):
+    advices = Advice_item.objects.all().order_by('-id')
+    serializer = Advice_itemSerializer(advices, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def order_add(request):
+    name = request.POST.get('name')
+    phone = request.POST.get('phone')
+    new_order = Order.objects.create(
+        name=name,
+        phone=phone,
+    )
+    serializer = OrderSerializer(new_order)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def instruction_view(request):
+    try:
+        instruction = Instruction.objects.last()
+        serializer = InstructionSerializer(instruction)
+        return Response(serializer.data)
+    except:
+        return Response({'message': 'None'})
+
+@api_view(['GET'])
+def info_view(request):
+    try:
+        info = Info.objects.last()
+        serializer = InfoSerializer(info)
+        return Response(serializer.data)
+    except:
+        return Response({'message': 'None'})
+
 
 
